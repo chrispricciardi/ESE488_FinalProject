d477fec35ea62833a5dfcc484068604179e16ce1
AlanCoe <32847430+AlanCoe@users.noreply.github.com>
2017-11-30 20:13:46 -0600

    Add files via upload

---
+++ mult_tb.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+
+module mult_tb();
+
+    reg  clk;
+    reg  reset;
+    reg [15:0] in;
+    reg [15:0] w;
+    wire [15:0] out;
+
+  
+
+    mult DUT(
+        .clk(clk),
+        .reset(reset),
+	.in(in),
+	.w(w),
+	.out(out)
+        );
+
+    initial
+    begin
+        $dumpfile("mult.vcd");
+        $dumpvars(0, mult_tb);
+
+        reset = 1;
+        clk = 0;
+	
+	in= 16'h1100;
+	w = 16'h1100;
+
+        // 1
+        #16
+        reset = 0;
+
+	
+
+
+        #300
+	//Let it run
+	
+        $finish;
+    end
+
+    always 
+        #10 clk = !clk;
+
+endmodule
